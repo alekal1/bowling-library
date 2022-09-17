92666eca0e63fb4733b30e0460d3457ecb4e17ca
alekal1 <sasha.aleksandov@gmail.com>
2022-09-17 15:58:46 +0300

    BowlingRoll bug fix

--- src/main/java/ee/alekal/bowlingscore/dto/BowlingRoll.java
+++ src/main/java/ee/alekal/bowlingscore/dto/BowlingRoll.java
@@ -1,10 +1,14 @@
 package ee.alekal.bowlingscore.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BowlingRoll {
     String playerNickname;
     String score;
